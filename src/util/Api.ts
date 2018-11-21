c5fc82c83b5ad16264cd4aa709976a50e7292c85
Gilbert Champion <gchampion@liveperson.com>
2018-11-20 20:15:52 -0500

    modify to work on server

--- src/util/Api.ts
+++ src/util/Api.ts
@@ -5,70 +5,70 @@ import { IPlug, Favorites } from './Interfaces';
 
 export default class Api {
   public async getLights() {
-    const response = await requestPromise.get('http://localhost:1981/lights', { json: true });
+    const response = await requestPromise.get('/lights', { json: true });
     return response.payload;
   }
   public async toggleLight(light: ILight) {
     const body = cloneDeep(light.state);
     body.on = !body.on;
     const options = { body, json: true };
-    const url = `http://localhost:1981/lights/${light.id}/state`;
+    const url = `/lights/${light.id}/state`;
     const response = await requestPromise.put(url, options);
     return response.payload;
   }
   public async editLight(light: ILight) {
     const options = { body: light, json: true };
-    const url = `http://localhost:1981/lights`;
+    const url = `/lights`;
     const response = await requestPromise.put(url, options);
     return response.payload;
   }
   public async getGroups() {
-    const response = await requestPromise.get('http://localhost:1981/groups', { json: true });
+    const response = await requestPromise.get('/groups', { json: true });
     return response.payload;
   }
   public async toggleGroup(group: ILightGroup) {
     const body = cloneDeep(group.action);
     body.on = !body.on;
     const options = { body, json: true };
-    const url = `http://localhost:1981/groups/${group.id}/action`;
+    const url = `/groups/${group.id}/action`;
     const response = await requestPromise.put(url, options);
     return response.payload;
   }
   public async editGroup(group: ILightGroup) {
     const options = { body: group, json: true };
-    const url = `http://localhost:1981/groups`;
+    const url = `/groups`;
     const response = await requestPromise.put(url, options);
     return response.payload;
   }
 
   public async getPlugs() {
-    const response = await requestPromise.get('http://localhost:1981/plugs', { json: true });
+    const response = await requestPromise.get('/plugs', { json: true });
     return response.payload;
   }
   public async editPlug(plug: IPlug) {
     const options = { body: plug, json: true };
-    const url = `http://localhost:1981/plugs`;
+    const url = `/plugs`;
     const response = await requestPromise.put(url, options);
     return response.payload;
   }
   public async togglePlug(plug: IPlug) {
     const body = { on: !plug.state.on };
     const options = { body, json: true };
-    const url = `http://localhost:1981/plugs/${plug.id}/state`;
+    const url = `/plugs/${plug.id}/state`;
     const response = await requestPromise.put(url, options);
     return response.payload;
   }
 
   public async getFavorites() {
     const response = await requestPromise.get(
-      'http://localhost:1981/favorites',
+      '/favorites',
       { json: true });
     return response.payload;
   }
 
   public async toggleFavorite(item: any) {
     const favorites: Favorites = await this.getFavorites();
-    const url = `http://localhost:1981/favorites/${item.deviceType}s/${item.id}`;
+    const url = `/favorites/${item.deviceType}s/${item.id}`;
     const currentFavorites = favorites[`${item.deviceType}s`];
     // const method = currentFavorites.indexOf(item.id) > -1 ? 'delete' : 'put';
     if (currentFavorites.indexOf(item.id) > -1) {
